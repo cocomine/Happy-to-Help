b264bf50421b187bb1f82fb3efbab026007a9454
cocomine <roguish.6888467468@gmail.com>
2018-07-19 20:20:34 +0800

    upload

---
+++ src/com/cocopixelmc/HappyToHelp/Main.java
@@ -0,0 +1,17 @@
+package com.cocopixelmc.HappyToHelp;
+
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin implements Listener{
+
+	@Override
+    public void onEnable() {
+		
+	}
+	
+	@Override
+    public void onDisable() {
+		
+	}
+}
