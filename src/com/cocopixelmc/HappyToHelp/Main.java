03baefc3c994a4d54b00cff3f463306a723ec751
cocomine <roguish.6888467468@gmail.com>
2018-07-19 21:42:32 +0800

    updata

--- src/com/cocopixelmc/HappyToHelp/Main.java
+++ src/com/cocopixelmc/HappyToHelp/Main.java
@@ -3,11 +3,16 @@ package com.cocopixelmc.HappyToHelp;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.comphenix.protocol.ProtocolLibrary;
+import com.comphenix.protocol.ProtocolManager;
+
 public class Main extends JavaPlugin implements Listener{
+
+	public static ProtocolManager protocolManager;
 
 	@Override
     public void onEnable() {
-		
+		protocolManager = ProtocolLibrary.getProtocolManager();
 	}
 	
 	@Override
